28aa7adc59a7ecde2d04427dd7776eab5ef75f0c
Paria <pariamakaremi20@gmail.com>
2018-02-05 18:09:51 -0500

    first commit

--- lib/Test/src/TDMASolverTest.cpp
+++ lib/Test/src/TDMASolverTest.cpp
@@ -12,6 +12,16 @@ using testing::Eq;
 namespace {
     struct TDMASolverTest :public testing::Test{
     public:
+        TDMASolver* DummyExample;
+        TDMASolverTest(){
+            DummyExample=new TDMASolver;
+        }
+
+        ~TDMASolverTest(){}
 
     };
+}
+
+TEST_F(TDMASolverTest,TEST0){
+    ASSERT_EQ(1,1);
 }

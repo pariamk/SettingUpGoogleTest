5e40288927424f6ed251974a01c4449da380340f
Paria <pariamakaremi20@gmail.com>
2018-02-07 10:37:49 -0500

    first commit

--- lib/Test/src/TDMASolverTest.cpp
+++ lib/Test/src/TDMASolverTest.cpp
@@ -7,6 +7,7 @@
 #include "../Include/TDMASolver.h"
 #include <vector>
 
+
 using testing::Eq;
 
 namespace {
@@ -21,7 +22,18 @@ namespace {
 
     };
 }
-
+/* dummy test for checking */
 TEST_F(TDMASolverTest,TEST0){
     ASSERT_EQ(1,1);
+}
+
+
+TEST_F(TDMASolverTest,TEST1){
+    std::vector<double> f;
+    f.resize(4,0.0);
+    ASSERT_EQ(f[1],0);
+}
+
+TEST_F(TDMASolverTest,TEST4x4Solver){
+
 }
